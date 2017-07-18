4b643b4be109e203ad757933fbba44784c62b009
Josh Steverman <jstever@umich.edu>
2017-07-18 12:52:31 -0400

    reprocess nauths

--- scripts/reprocess_nauths.rb
+++ scripts/reprocess_nauths.rb
@@ -5,6 +5,10 @@ Mongoid.load!("config/mongoid.yml", :production)
 Authority = Nauth::Authority
 
 Authority.where(marc:{"$exists":1}).no_timeout.each do | auth |
-  auth.marc = auth.marc
+  #auth.marc = auth.marc
+  auth.relationships
+  auth.alternate_names
+  auth.termination_date
+  auth.establishment_date
   auth.save
 end
