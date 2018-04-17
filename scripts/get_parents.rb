c4f63158570677d100cc5ed047efcad4780d501c
Josh Steverman <jstever@umich.edu>
2018-04-17 13:08:07 -0400

    get a record and its parents with its lccn uri

---
+++ scripts/get_parents.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../lib/nauth'
+require 'dotenv'
+Dotenv.load
+Mongoid.load!('config/mongoid.yml', :production)
+Mongo::Logger.logger.level = ::Logger::FATAL
+Authority = Nauth::Authority
+
+
+same_as = ARGV.shift
+Authority.where(sameAs: same_as).each do |a|
+  PP.pp a.marc
+  PP.pp a.parents
+end
